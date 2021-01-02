87d0628ecd2aaa9d049479590df89b749f86f971
Robbie Heath <rmgheath1@sheffield.ac.uk>
2021-01-02 18:32:42 +0000

    Mess with the example bot

--- src/bot.py
+++ src/bot.py
@@ -11,6 +11,8 @@ class MyBot(BaseAgent):
     def initialize_agent(self):
         # This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
+        self.sign: float = (-1 if self.team else 1)
+        self.go_home: bool = False
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         ball_location = Vec3(packet.game_ball.physics.location)
@@ -18,26 +20,52 @@ class MyBot(BaseAgent):
         my_car = packet.game_cars[self.index]
         car_location = Vec3(my_car.physics.location)
 
-        car_to_ball = ball_location - car_location
+        if self.go_home:
+            self.go_home = car_location.y * self.sign > -4000
+        if not self.go_home:
+            self.go_home = car_location.y * self.sign > ball_location.y * self.sign
+        # self.go_home &= not packet.game_info.is_kickoff_pause
+
+        goal_location = Vec3(0, 5120 * self.sign, 0)
+        target_location = goal_location * -1
+        if not self.go_home:
+            shoot_factor: float = 0.3
+            target_location = ball_location - (
+                goal_location - ball_location
+            ).normalized * max(
+                130,
+                car_location.dist(ball_location)
+                * shoot_factor
+                * (not packet.game_info.is_kickoff_pause),
+            )
 
         # Find the direction of our car using the Orientation class
         car_orientation = Orientation(my_car.physics.rotation)
-        car_direction = car_orientation.forward
 
-        steer_correction_radians = find_correction(car_direction, car_to_ball)
+        steer_correction_radians = find_correction(
+            car_orientation.forward, target_location - car_location
+        )
 
-        if steer_correction_radians > 0:
-            # Positive radians in the unit circle is a turn to the left.
-            turn = -1.0  # Negative value for a turn to the left.
-            action_display = "turn left"
-        else:
-            turn = 1.0
-            action_display = "turn right"
+        self.controller_state.steer = max(-1, min(1, steer_correction_radians * -3))
+        self.controller_state.throttle = math.cos(steer_correction_radians)
+        self.controller_state.yaw = self.controller_state.steer * 0.6
+        self.controller_state.pitch = car_orientation.pitch * -0.6
+        self.controller_state.roll = car_orientation.roll * -0.6
+        self.controller_state.boost = (
+            self.controller_state.throttle > 0.85 and not my_car.is_super_sonic
+        )
+        self.controller_state.handbrake = abs(self.controller_state.throttle) < 0.5
+        self.controller_state.jump = (
+            my_car.has_wheel_contact and abs(steer_correction_radians) > 1
+        )
+        if not (my_car.has_wheel_contact or my_car.double_jumped):
+            dodge: bool = 0 < my_car.physics.velocity.z < 650 / 60
+            if dodge:
+                self.controller_state.jump = True
+                self.controller_state.pitch = -math.cos(steer_correction_radians)
+                self.controller_state.yaw = -math.sin(steer_correction_radians)
 
-        self.controller_state.throttle = 1.0
-        self.controller_state.steer = turn
-
-        draw_debug(self.renderer, my_car, packet.game_ball, action_display)
+        draw_debug(self.renderer, my_car, packet.game_ball, str(self.go_home))
 
         return self.controller_state
 
