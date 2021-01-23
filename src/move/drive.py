c069ae8dfce21c45848906e25657b524c4b38028
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-01-23 21:17:43 +0100

    Write a drive speed controller

--- src/move/drive.py
+++ src/move/drive.py
@@ -1,9 +1,18 @@
 from math import exp, atan2
+from typing import Tuple
 
 from rlbot.utils.rendering.rendering_manager import RenderingManager
 
 from move.move import Move
-from utils.const import MAX_CAR_SPEED
+from utils.const import (
+    BOOST_ACC,
+    BREAK_ACC,
+    COAST_ACC,
+    MAX_CAR_SPEED,
+    MIN_BOOST_TIME,
+    MAX_NO_BOOST_SPEED,
+    throttle_acc_from_speed,
+)
 from utils.game_info import GameInfo
 from rlutilities.linear_algebra import dot, norm, vec3
 
@@ -26,11 +35,11 @@ class Drive(Move):
 
         # TODO A proper speed controller.
         speed = norm(car.velocity)
-        self.controls.throttle = 1.0 if speed < self.target_speed else 0.0
+        self.controls.throttle, boost = speed_controller(
+            speed, self.target_speed, self.info.time_delta
+        )
         self.controls.boost = (
-            speed < self.target_speed
-            and abs_angle < MAX_BOOST_ANGLE
-            and speed < MAX_CAR_SPEED
+            boost and abs_angle < MAX_BOOST_ANGLE and speed < MAX_CAR_SPEED
         )
         self.controls.steer = 2 / (1 + exp(-5.0 * angle)) - 1
         self.controls.handbrake = abs_angle > HANDBRAKE_ANGLE
@@ -40,3 +49,50 @@ class Drive(Move):
     def render(self, r: RenderingManager):
         r.draw_rect_3d(self.target, 3, 3, True, r.red())
         r.draw_line_3d(self.info.my_car.position, self.target, r.white())
+
+
+def speed_controller(
+    current_vel: float, desired_vel: float, dt: float
+) -> Tuple[float, bool]:
+    throttle = 0.02
+    boost = False
+
+    if dt <= 0.0:
+        return throttle, boost
+
+    desired_vel = max(-MAX_NO_BOOST_SPEED, min(MAX_CAR_SPEED, desired_vel))
+    dv = desired_vel - current_vel
+    desired_acc = dv / dt
+
+    if desired_vel >= 0.0:
+        throttle_acc = throttle_acc_from_speed(current_vel)
+
+        if dv > (throttle_acc + BOOST_ACC) * MIN_BOOST_TIME:
+            throttle = 1.0
+            boost = True
+        elif desired_acc >= throttle_acc:
+            throttle = 1.0
+        elif 0 < desired_acc:
+            throttle = desired_acc / throttle_acc
+        elif -COAST_ACC < desired_acc:
+            pass
+        elif -BREAK_ACC < desired_acc:
+            throttle = 0.0
+        else:
+            throttle = -1.0
+
+    else:
+        throttle_acc = -throttle_acc_from_speed(-current_vel)
+
+        if desired_acc <= throttle_acc:
+            throttle = -1.0
+        elif 0 > desired_acc:
+            throttle = -desired_acc / throttle_acc
+        elif COAST_ACC > desired_acc:
+            throttle = -0.02
+        elif BREAK_ACC > desired_acc:
+            throttle = 0.0
+        else:
+            throttle = 1.0
+
+    return throttle, boost
--- src/utils/const.py
+++ src/utils/const.py
@@ -1,1 +1,18 @@
 MAX_CAR_SPEED = 2300
+MAX_NO_BOOST_SPEED = 1410
+BOOST_ACC = 991.667
+COAST_ACC = 525.0
+BREAK_ACC = 3500
+MIN_BOOST_TIME = 0.1
+
+
+def throttle_acc_from_speed(speed):
+    # https://samuelpmish.github.io/notes/RocketLeague/ground_control/#throttle
+    if speed < 0:
+        return BREAK_ACC
+    if speed < 1400:
+        return (-36 / 35) * speed + 1600
+    elif speed < 1410:
+        return -16 * speed + 22560
+    else:
+        return 0
