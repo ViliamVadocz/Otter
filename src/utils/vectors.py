b6eef17e9a37871f8f001fd3990160e1b1a61c72
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-01-23 21:14:55 +0100

    Add return types

--- src/utils/vectors.py
+++ src/utils/vectors.py
@@ -1,14 +1,13 @@
 from rlutilities.linear_algebra import dot, norm, vec3
 
 
-def dist(a: vec3, b: vec3):
+def dist(a: vec3, b: vec3) -> float:
     return norm(a - b)
 
 
-def flatten_by_normal(vec: vec3, normal: vec3):
+def flatten_by_normal(vec: vec3, normal: vec3) -> vec3:
     return vec - dot(vec, normal) * normal
 
 
-# TODO I was very tired when I wrote this and I am not sure if it is correct
-def flatten_by_normal_to_level(vec: vec3, normal: vec3, pos: vec3):
+def flatten_by_normal_to_level(vec: vec3, normal: vec3, pos: vec3) -> vec3:
     return vec + dot(pos - vec, normal) * normal
