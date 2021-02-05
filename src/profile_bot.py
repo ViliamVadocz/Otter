31608eeada0b1a8262047b95b8f65e0570f95a92
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-05 10:33:46 +0100

    Improve profiling

--- src/profile_bot.py
+++ src/profile_bot.py
@@ -1,30 +1,50 @@
+import os
 import sys
 import cProfile
+from pstats import Stats, SortKey
+from shutil import rmtree
 from pathlib import Path
 
+from rlbot.agents.base_agent import SimpleControllerState
 from rlbot.agents.standalone.standalone_bot import StandaloneBot, run_bot
 
 from bot import Otter
 
-
-class OtterProfileBot(StandaloneBot):
-    def is_hot_reload_enabled(self):
-        return False
-
-    initialize_agent = Otter.initialize_agent
-    get_output = Otter.get_output
-    pick_strategy = Otter.pick_strategy
-    render = Otter.render
+PROFILE_DATA_DIR = (Path(__file__).parent / "profile_data").absolute()
+COMBINED_FILE = PROFILE_DATA_DIR / "combined"
+MAX_FRAMES = 10_000
 
 
-def main():
-    try:
-        run_bot(OtterProfileBot)
-    except Exception as err:
-        print(err)
+class OtterProfileBot(Otter, StandaloneBot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.frame = 0
+
+    def get_output(self, game_tick_packet):
+        if self.frame >= MAX_FRAMES:
+            print("Profiling done, interrupt this with Ctrl+C")
+            return SimpleControllerState()
+        pr = cProfile.Profile()
+        pr.enable()
+        output = super().get_output(game_tick_packet)
+        pr.disable()
+        pr.create_stats()
+        pr.dump_stats(PROFILE_DATA_DIR / str(self.frame))
+        self.frame += 1
+        return output
 
 
 if __name__ == "__main__":
+
+    if os.path.isfile(COMBINED_FILE):
+        ps = Stats()
+        ps.add(str(COMBINED_FILE))
+        ps.sort_stats(SortKey.TIME).print_stats()
+        exit()
+
+    if os.path.isdir(PROFILE_DATA_DIR):
+        rmtree(PROFILE_DATA_DIR)
+    os.mkdir(PROFILE_DATA_DIR)
 
     config_path = (Path(__file__).parent / "bot.cfg").absolute()
     sys.argv[1:] = [
@@ -33,4 +53,17 @@ if __name__ == "__main__":
         "--team=0",
         "--spawn-id=1",
     ]
-    cProfile.run("main()")
+    try:
+        run_bot(OtterProfileBot)
+    except Exception as err:
+        print(err)
+
+    print("Combining frame profile data")
+    ps = Stats()
+    for file in os.listdir(PROFILE_DATA_DIR):
+        path_to_file = PROFILE_DATA_DIR / file
+        if os.path.isfile(path_to_file):
+            ps.add(str(path_to_file))
+            os.remove(path_to_file)
+    ps.dump_stats(str(COMBINED_FILE))
+    ps.sort_stats(SortKey.TIME).print_stats()
