564fb5a4b5ee761cdf17331361a55362a2a532d4
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-13 11:22:13 +0100

    Add new vector functions

--- src/utils/vectors.py
+++ src/utils/vectors.py
@@ -11,3 +11,13 @@ def flatten_by_normal(vec: vec3, normal: vec3) -> vec3:
 
 def direction(start: vec3, end: vec3) -> vec3:
     return normalize(end - start)
+
+
+def flat_direction(start: vec3, end: vec3, normal: vec3) -> vec3:
+    return normalize(flatten_by_normal(end - start, normal))
+
+
+def alignment(start: vec3, middle: vec3, end: vec3, normal: vec3) -> float:
+    return dot(
+        flat_direction(start, middle, normal), flat_direction(middle, end, normal)
+    )
