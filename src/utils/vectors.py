04511b047a3b4583fe21a083d23a0ae813a36f32
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-14 12:52:11 +0100

    Add method for generating orientation from up dir

--- src/utils/vectors.py
+++ src/utils/vectors.py
@@ -1,4 +1,4 @@
-from rlutilities.linear_algebra import dot, norm, vec3, normalize
+from rlutilities.linear_algebra import dot, mat3, norm, vec3, cross, normalize
 
 
 def dist(a: vec3, b: vec3) -> float:
@@ -21,3 +21,17 @@ def alignment(start: vec3, middle: vec3, end: vec3, normal: vec3) -> float:
     return dot(
         flat_direction(start, middle, normal), flat_direction(middle, end, normal)
     )
+
+
+def up_at(up: vec3, forward: vec3) -> mat3:
+    up = normalize(up)
+    left = normalize(cross(up, forward))
+    forward = normalize(cross(left, up))
+
+    # fmt: off
+    return mat3(
+        forward[0], left[0], up[0],
+        forward[1], left[1], up[1],
+        forward[2], left[2], up[2],
+    )
+    # fmt: on
