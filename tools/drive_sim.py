5dc39603106cb78c0bee19dfeaa017d530d8cac8
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-14 10:02:38 +0100

    Move drive_sim to tools

--- tools/drive_sim.py
+++ tools/drive_sim.py
@@ -1,7 +1,16 @@
-from const import (
+import sys
+from math import sin, sqrt
+
+import matplotlib.pyplot as plt
+
+sys.path.insert(1, "../src")
+
+from move.drive import speed_controller as drive_speed_controller
+from utils.const import (
     BOOST_ACC,
     BREAK_ACC,
     COAST_ACC,
+    BOOST_USAGE,
     MAX_CAR_SPEED,
     MIN_BOOST_TIME,
     throttle_acc_from_speed,
@@ -52,33 +61,60 @@ class Sim:
 
     @property
     def boost_used(self):
-        return self.boost_use * (100.0 / 3.0)
+        return self.boost_use * BOOST_USAGE
 
 
 if __name__ == "__main__":
-    from math import sin
-
-    import matplotlib.pyplot as plt
-
     # Replace with your own speed controller!
     def speed_controller(u, v, _dt):
         throttle = (v - u) / 50
         boost = throttle > 1.0
         return throttle, boost
 
-    sim = Sim()
     dt = 1 / 120
-    seconds = 6
-    t = [i * dt for i in range(int(seconds * (1 / dt)))]
-    a = []
-    b = []
-    for time in t:
-        target = sin(time) * 2000
-        a.append(target)
-        b.append(sim.vel)
-        throttle, boost = speed_controller(sim.vel, target, dt)
-        sim.step(dt, throttle, boost)
-    print("boost used:", sim.boost_used)
-    plt.plot(t, a)
-    plt.plot(t, b)
+    seconds = 10
+
+    sim1 = Sim()
+    sim2 = Sim()
+    times = [i * dt for i in range(int(seconds * (1 / dt)))]
+    targets = [
+        sin(time * 12) * 200 - sin(time * 8) * 300 + sin(time) * 500 for time in times
+    ]
+    simple_vel = []
+    simple_err = []
+    drive_vel = []
+    drive_err = []
+    for time, target in zip(times, targets):
+        simple_vel.append(sim1.vel)
+        drive_vel.append(sim2.vel)
+
+        throttle, boost = speed_controller(sim1.vel, target, dt)
+        sim1.step(dt, throttle, boost)
+        throttle, boost = drive_speed_controller(sim2.vel, target, dt)
+        sim2.step(dt, throttle, boost)
+
+        simple_err.append(target - sim1.vel)
+        drive_err.append(target - sim2.vel)
+
+    print("[Simple] boost used:", sim1.boost_used)
+    print("[Drive] boost used:", sim2.boost_used)
+
+    print(
+        "[Simple] std dev",
+        sqrt(sum(map(lambda err: err * err, simple_err)) / len(simple_err)),
+    )
+    print(
+        "[Drive] std dev",
+        sqrt(sum(map(lambda err: err * err, drive_err)) / len(drive_err)),
+    )
+
+    plt.plot(times, targets)
+    plt.plot(times, simple_vel)
+    plt.plot(times, drive_vel)
+    plt.show()
+
+    plt.plot(times, targets)
+    plt.plot(times, simple_err)
+    plt.plot(times, drive_err)
+    plt.plot(times, [0 for t in times])
     plt.show()
