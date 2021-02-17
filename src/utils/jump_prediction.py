e02793bc1a2799e209624bc090707ede820af956
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-17 19:09:18 +0100

    Add solver for double jumps

--- src/utils/jump_prediction.py
+++ src/utils/jump_prediction.py
@@ -1,5 +1,5 @@
 from math import inf
-from typing import Tuple
+from typing import Any, Tuple, Optional, Collection
 
 import numpy as np
 
@@ -42,6 +42,34 @@ def predict_double_jump(car: Car, grav: vec3, time: float) -> vec3:
     return pos + vel * fall_time + 0.5 * grav * fall_time * fall_time
 
 
+def get_roots(pos: vec3, vel: vec3, acc: vec3, target: vec3) -> Collection[Any]:
+    # We want to find when the distance to target is smallest.
+    # let f(t) = pos + vel * t + 0.5 * acc * time^2 - target
+    # We find the minimum of ||f(t)||.
+    # d(||f(t)||^2) / dt = 2 * f'(t) . f(t)
+    # Set derivative equal to zero and you get a cubic in t.
+    a = 0.5 * dot(acc, acc)
+    b = 1.5 * dot(acc, vel)
+    c = dot(vel, vel) + dot(acc, pos - target)
+    d = dot(vel, pos - target)
+    return np.roots([a, b, c, d])
+
+
+def smallest_real_root_in_range(
+    roots, minimum: float, maximum: float
+) -> Optional[float]:
+    best_time = None
+    for root in roots:
+        maybe_real = np.real_if_close(root)
+        if (
+            np.isreal(maybe_real)
+            and minimum <= maybe_real < maximum
+            and (best_time is None or maybe_real < best_time)
+        ):
+            best_time = maybe_real
+    return best_time
+
+
 def solve_jump(car: Car, grav: vec3, target: vec3) -> Tuple[vec3, float]:
     up = car.up()
     # Assume closest is after first jump. (time >= MAX_FIRST_JUMP_HOLD)
@@ -50,24 +78,25 @@ def solve_jump(car: Car, grav: vec3, target: vec3) -> Tuple[vec3, float]:
         grav + up * JUMP_ACC
     ) * MAX_FIRST_JUMP_HOLD
     acc = grav
-
-    # We want to find when the distance to target is smallest,
-    # we find the minimum of f(t).
-    # f(t) = pos + vel * t + 0.5 * acc * time^2 - target
-    # d(||f(t)||^2) / dt = 2 * f'(t) . f(t)
-    # Set derivative equal to zero and you get a cubic in t:
-    a = 0.5 * dot(acc, acc)
-    b = 1.5 * dot(acc, vel)
-    c = dot(vel, vel) + dot(acc, pos - target)
-    d = dot(vel, pos - target)
-
-    # Find smallest real time which is >= 0.0.
-    roots = np.roots([a, b, c, d])
-    best_time = inf
-    for root in roots:
-        maybe_real = np.real_if_close(root)
-        if np.isreal(maybe_real):
-            maybe_real += MAX_FIRST_JUMP_HOLD
-            if 0.0 <= maybe_real < best_time:
-                best_time = maybe_real
+    roots = get_roots(pos, vel, acc, target)
+    best_time = (
+        smallest_real_root_in_range(roots, -MAX_FIRST_JUMP_HOLD, inf)
+        + MAX_FIRST_JUMP_HOLD
+    )
     return predict_jump(car, grav, best_time) - target, best_time
+
+
+def solve_double_jump(car: Car, grav: vec3, target: vec3) -> Tuple[vec3, float]:
+    up = car.up()
+    # Assume closest is after first jump. (time >= MAX_FIRST_JUMP_HOLD)
+    pos = predict_double_jump(car, grav, MAX_FIRST_JUMP_HOLD)
+    vel = (car.velocity + up * (JUMP_IMPULSE + DOUBLE_JUMP_IMPULSE)) + (
+        grav + up * JUMP_ACC
+    ) * MAX_FIRST_JUMP_HOLD
+    acc = grav
+    roots = get_roots(pos, vel, acc, target)
+    best_time = (
+        smallest_real_root_in_range(roots, -MAX_FIRST_JUMP_HOLD, inf)
+        + MAX_FIRST_JUMP_HOLD
+    )
+    return predict_double_jump(car, grav, best_time) - target, best_time
